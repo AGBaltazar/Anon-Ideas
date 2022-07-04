7ccef1259aeb4775a5dcd6a8cd320275c2554780
Alejandro Garcia Baltazar <agee.baltazar@gmail.com>
2022-07-04 08:37:33 -0700

    Added backbone for frontend

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,7 +1,7 @@
 import PostMessage from "../models/postMessage";
 
 //The post controller will aid in making a simple logic function 
-//that can be exported to our routes
+//that will be called from the front end
 
 //Due to the time it will take to find messages from within our models, this will be a async function 
 
@@ -9,14 +9,22 @@ export const getPosts = async (req, res) => {
     try{
         const postMessages = await PostMessage.find();
 
-        console.log(postMessages);
-
         res.status(200).json(postMessages);
     } catch(error){
         res.status(404).json({message: error.message});
     }
 }
 
-export const createPost = (req, res) =>{
-    res.send('Post Creation');
+export const createPost = async (req, res) =>{
+   const post = req.body;
+
+   const newPost = new PostMessage(post);
+
+    try{
+        await newPost.save();
+
+        res.status(201).json(newPost);
+    } catch(error){
+        res.status(404).json({message: error.message});
+    }
 }
